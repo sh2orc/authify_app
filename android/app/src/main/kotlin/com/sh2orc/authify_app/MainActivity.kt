c572f9edabde335fae50279837684b1bf60211b4
sh2orc <sh2orc@gmail.com>
2023-07-06 22:45:11 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/sh2orc/authify_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sh2orc.authify_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+
+  double _deviceHeight=0.0;
+  double _deviceWidth=0.0;
+
+  Color _primaryColor = Color.fromRGBO(125, 191, 211, 1.0);
+  Color _secondaryColor = Color.fromRGBO(169, 224, 241, 1.0);
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: _primaryColor,
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          height: _deviceHeight * 0.60,
+          width: _deviceWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _avatarWidget(),
+              SizedBox(height: _deviceHeight * 0.05,),
+              _emailTextField(),
+              _passwordTextField(),
+              SizedBox(height: _deviceHeight * 0.10,),
+              _loginButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _avatarWidget(){
+    double _circleD = _deviceHeight * 0.25;
+    return Container(
+      height: _circleD,
+      width: _circleD,
+      decoration: BoxDecoration(
+          color: _secondaryColor,
+          borderRadius: BorderRadius.circular(500),
+          image: DecorationImage(
+            image: AssetImage('assets/images/main_avatar.png'),
+          )),
+    );
+  }
+
+  Widget _emailTextField(){
+    return Container(
+      width: _deviceWidth * 0.70,
+      child: TextField(
+        cursorColor: Colors.white,
+        autocorrect: false,
+        style: TextStyle(color: Colors.white,),
+        decoration: InputDecoration(
+          hintText: "sh2orc@gmail.com",
+          hintStyle: TextStyle(color: Colors.white),
+          enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)
+          )
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordTextField(){
+    return Container(
+      width: _deviceWidth * 0.70,
+      child: TextField(
+        cursorColor: Colors.white,
+        autocorrect: false,
+        style: TextStyle(color: Colors.white,),
+        decoration: InputDecoration(
+            hintText: "password",
+            hintStyle: TextStyle(color: Colors.white),
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)
+            ),
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)
+            )
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton(){
+    return MaterialButton(
+      minWidth: _deviceWidth * 0.38,
+      height: _deviceHeight * 0.055,
+      color: Colors.white,
+      child: Text(
+        "LOG IN",
+        style: TextStyle(
+            fontSize: 16, color: _primaryColor, fontWeight: FontWeight.bold),
+      ),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+          side: BorderSide(color: Colors.white),
+      ),
+      onPressed: (){
+        
+      },
+    );
+  }
+
+}
